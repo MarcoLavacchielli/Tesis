d82b5e3af3a48c092ef209d3da13898e07181807
Marco <marco.lavacchielli@davinci.edu.ar>
2024-04-08 19:36:22 -0300

    ya funcionan las 2 trampas

--- Assets/Laser.cs
+++ Assets/Laser.cs
@@ -2,29 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpikeTrap : Trap
+public class Laser : Trap
 {
-    public Animator spikeTrap;
-
-    public int damageAmount = 5;
+    public int damageAmount = 10;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            spikeTrap.Play("Spike Trap (Abrir)");
             Activate();
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        spikeTrap.Play("Spike Trap (Cerrar)");
-    }
-
     public override void Activate()
     {
-        Debug.Log("Spike trap activated!");
+        Debug.Log("Player touch laser!");
 
         PlayerHealth playerHealth = FindObjectOfType<PlayerHealth>();
         if (playerHealth != null)
