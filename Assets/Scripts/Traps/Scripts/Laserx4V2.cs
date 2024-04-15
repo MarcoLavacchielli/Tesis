70135a65d7cdf58cc3a2c77cd05f4d867eccdb6a
Marco <marco.lavacchielli@davinci.edu.ar>
2024-04-14 22:14:15 -0300

    Se creo nueva trampa laser

--- Assets/Scripts/Traps/Scripts/Laserx4V2.cs
+++ Assets/Scripts/Traps/Scripts/Laserx4V2.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Laserx4 : MonoBehaviour
+public class Laserx4V2 : MonoBehaviour
 {
     public float velocidadRotacion;
 
     void Update()
     {
-        transform.Rotate(Vector3.up, velocidadRotacion * Time.deltaTime);
+        transform.Rotate(Vector3.right, velocidadRotacion * Time.deltaTime);
     }
 }
