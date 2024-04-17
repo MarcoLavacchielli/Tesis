43db6c3ef7d7c626330c8dc5c524c36105869ada
Marco <marco.lavacchielli@davinci.edu.ar>
2024-04-17 17:46:45 -0300

    la trampa de pinchos ya no va por animacion

--- Assets/Scripts/Traps/Scripts/SpikeTrap.cs
+++ Assets/Scripts/Traps/Scripts/SpikeTrap.cs
@@ -4,22 +4,48 @@ using UnityEngine;
 
 public class SpikeTrap : Trap
 {
-    public Animator spikeTrap;
-
+    public Transform spikeTransform;
+    public float moveDistance = 1f;
+    public float moveSpeed = 1f;
     public int damageAmount;
+
+    private bool playerInside = false;
+    private Vector3 initialPosition;
+    private Vector3 targetPosition;
+
+    private void Start()
+    {
+        initialPosition = spikeTransform.position;
+        targetPosition = initialPosition + Vector3.up * moveDistance;
+    }
+
+    private void Update()
+    {
+        if (playerInside)
+        {
+            spikeTransform.position = Vector3.MoveTowards(spikeTransform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            spikeTransform.position = Vector3.MoveTowards(spikeTransform.position, initialPosition, moveSpeed * Time.deltaTime);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            spikeTrap.Play("Spike Trap (Abrir)");
+            playerInside = true;
             Activate();
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        spikeTrap.Play("Spike Trap (Cerrar)");
+        if (other.CompareTag("Player"))
+        {
+            playerInside = false;
+        }
     }
 
     public override void Activate()
