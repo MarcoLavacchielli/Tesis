a0640aaec55d87fe6420135791fea13d20ea7654
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2024-05-08 01:22:45 -0300

    menu principal y de pausa

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseCanvas;
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            Time.timeScale = 0f;
+            pauseCanvas.SetActive(true);
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            Time.timeScale = 1f;
+            pauseCanvas.SetActive(false);
+        }
+    }
+
+    public void Loadscene(string scenename)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(scenename);
+    }
+}
