923111b3ec85911cd3bb9ce88ef8ef1e9e703b73
facutise <facundo.tisera123@gmail.com>
2024-06-11 13:08:52 -0300

    disparo con instantiate

--- Assets/Scripts/Proyectiles/Pistol.cs
+++ Assets/Scripts/Proyectiles/Pistol.cs
@@ -9,7 +9,7 @@ public class Pistol : MonoBehaviour
     public float spread;
     public float shootForce, upwardForce;
 
-
+    public GameObject _bullet;
 
 
 
@@ -20,7 +20,42 @@ public class Pistol : MonoBehaviour
 
         if (!InputsScript) return;
 
-        InputsScript.ShootPistolFuncInput += shoot;
+        InputsScript.ShootPistolFuncInput += Shoot2;
+
+
+    }
+    public void Shoot2()
+    {
+        //Find the exact hit position using a raycast
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); //Just a ray through the middle of your current view
+        RaycastHit hit;
+        //check if ray hits something
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else
+            targetPoint = ray.GetPoint(75); //Just a point far away from the player
+
+        //Calculate direction from attackPoint to targetPoint
+        Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
+
+        //Calculate spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+
+
+        //Calculate new direction with spread
+        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0); //Just add spread to last direction
+
+
+        //Instantiate bullet/projectile
+        GameObject currentBullet = Instantiate(_bullet, attackPoint.position, Quaternion.identity); //store instantiated bullet in currentBullet
+        //Rotate bullet to shoot direction
+        currentBullet.transform.forward = directionWithSpread.normalized;
+        //Add forces to bullet
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
+        currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
 
 
     }
