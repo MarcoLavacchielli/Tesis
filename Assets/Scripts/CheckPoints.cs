d73afa4612e0a62dc16bdbd08b06f7af0d2fc6d8
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2024-11-02 21:51:25 -0300

    Un checkpoint establecido

---
+++ Assets/Scripts/CheckPoints.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoints : MonoBehaviour
+{
+
+    public CheckTeleporter check;
+
+    // Referencia al objeto feedback
+    public GameObject targetObject;
+    public Material targetMaterial;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            check.currentWaypointIndex += 1;
+
+            // Cambiar el material del objeto al material especificado
+            if (targetObject != null && targetMaterial != null)
+            {
+                Renderer renderer = targetObject.GetComponent<Renderer>();
+                if (renderer != null)
+                {
+                    renderer.material = targetMaterial;
+                }
+            }
+
+            Destroy(gameObject);
+        }
+    }
+
+}
